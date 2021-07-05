6562f36489b3b74f22d2571d6a8ea9e3a72f67ae
Luisa Ghellere <54870171+ghellereluisa@users.noreply.github.com>
2021-07-05 17:26:54 -0300

    uri

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   }
   async function fetch() {
     const response = await axios
-      .get("uri backend")
+      .get("https:localhost:8080/receitas")
       .then((response) => response.data);
     setReceitas(response);
     console.log(response);
