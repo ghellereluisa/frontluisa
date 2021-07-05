fd6a2387ef4ec710a6b2fabe014ef47938496374
Luisa Ghellere <54870171+ghellereluisa@users.noreply.github.com>
2021-07-05 17:20:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
 
 function App() {
+  const [receitas, setReceitas] = React.useState([]);
+  const [carregando, setCarregando] = React.useState(null);
+  async function handleClick() {
+    setCarregando(true);
+    receitas.length === 0 ? await fetch() : setReceitas([]);
+    setCarregando(false);
+  }
+  async function fetch() {
+    const response = await axios
+      .get("uri backend")
+      .then((response) => response.data);
+    setReceitas(response);
+    console.log(response);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>Click for receitas</button>
+
+      {carregando ? (
+        <p>carregando...</p>
+      ) : (
+        <>
+          {receitas.map((receita) => (
+            <ul>
+              <li key={receita.id}>
+                Nome: {receita.nome}
+                Valor: {receita.valor}
+                Descrição: {receita.descricao}
+                Usuário: {receita.usuario}
+              </li>
+            </ul>
+          ))}
+        </>
+      )}
     </div>
   );
 }
